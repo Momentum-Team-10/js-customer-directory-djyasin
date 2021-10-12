8413cf65330ff1fdc8e505e60bd5a7fd32993ef7
Lila <djyasin.ly@gmail.com>
2021-10-12 16:09:51 -0400

    Added some js, backing up to redefine customer.js

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-console.log(products)
-console.log(states)
+console.log(customers);
+//console.log(states);
 
+const container = document.createElement("div");
+root.appendChild(container);
+container.classList.add("uk-container-large", "uk-flex", "uk-flex-between");
 
-function tableFor(event, journal) {
-    let table = [0, 0, 0, 0];
-    for (let i = 0; i < journal.length; i++) {
-      let entry = journal[i], index = 0;
-      if (entry.events.includes(event)) index += 1;
-      if (entry.squirrel) index += 2;
-      table[index] += 1;
-    }
-    return table;
-  }
+for (let customer of customers) {
+  const customerCard = document.createElement("div");
+  customerCard.id = `${email.id}`;
+  container.appendChild(customerCard);
+  customerCard.classList.add(
+    "uk-card",
+    "uk-card-body",
+    "uk-card-default",
+    "uk-width-medium"
+  );
+
+  let title = document.createElement("p");
+  title.innerText = customer.title;
+  customerCard.appendChild(title);
+
+  let templateLiteral = `${customer.name}`;
+  console.log(templateLiteral);
+
+  customerCard.innerHTML = `
+  <p class="uk-card-title" style="color: red !important; padding: 15px">${
+    customer.title
+  }</p>
+  <img src=${customer.thumbnail} />
+  <p>${customer.email}</p>
+  <p>$${customer.age}</p>
+  <p>${customer.location}</p>
+ 
+`;
+}
