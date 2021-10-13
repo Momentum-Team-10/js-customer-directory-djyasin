4343b6ae1f9280db7a589d87aa55ce16479ed78e
Lila <djyasin.ly@gmail.com>
2021-10-12 20:26:48 -0400

    Got most p tags working. Need to get photo and customer since moment working.

--- main.js
+++ main.js
@@ -4,26 +4,29 @@ root.appendChild(container);
 container.classList.add("uk-container-large", "uk-flex", "uk-flex-between");
 
 for (let customer of customers) {
-const customerCard = document.createElement("div");
-customerCard.id = `${customer.id.value}`;
-container.appendChild(customerCard);
-customerCard.classList.add(
-  "uk-card",
-  "uk-card-body",
-  "uk-card-default",
-  "uk-width-medium"
+  const customerCard = document.createElement("div");
+  customerCard.id = `${customer.id.value}`;
+  container.appendChild(customerCard);
+  customerCard.classList.add(
+    "uk-card",
+    "uk-card-body",
+    "uk-card-default",
+    "uk-width-medium"
   );
-customerCard.innerHTML = `
-  <p class="uk-card-title" style="color: red !important; padding: 15px">${
-   customer.name
-}
+  customerCard.innerHTML = `
+  <p class="uk-card-title" style="color: red !important; padding: 15px">${customer.name}
 <img src=${customer.thumbnail}/>
- <p>${customer.email}</p>
- <p>${customer.age}</p>
- <p>${customer.location.street}</p>
- <p>${customer.location.street.number}</p>
- 
+<p><h3>${customer.name.first}</h3></p>
+<p><h3>${customer.name.last}</h3></p>
+<p>${customer.email}</p>
+<p>${customer.location.street.number}</p>
+<p>${customer.location.street.name}</p> 
+<p>${customer.location.city}</p>
+<p>${customer.location.state}</p>
+<p>${customer.location.postcode}</p>
+<p>DOB: ${customer.dob.age}</p>
+<p>Customer Since: ${customer.since}</p>
 `;
 }
-let customer = customers[0]
-console.log(customer)
+let customer = customers[0];
+console.log(customer);
